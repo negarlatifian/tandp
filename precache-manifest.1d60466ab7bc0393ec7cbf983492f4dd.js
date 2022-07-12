b508b2b2d39b383577ef2afafa7a2837efb29fb7
negarlatifian <74406918+negarlatifian@users.noreply.github.com>
2022-07-13 00:18:43 +0100

    Updates

---
+++ precache-manifest.1d60466ab7bc0393ec7cbf983492f4dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa8f2f1a3444f0566819",
+    "url": "/tandp/static/css/main.27959439.chunk.css"
+  },
+  {
+    "revision": "aa8f2f1a3444f0566819",
+    "url": "/tandp/static/js/main.aa8f2f1a.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/tandp/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "c3a0ac93f1a0b052545b",
+    "url": "/tandp/static/js/2.c3a0ac93.chunk.js"
+  },
+  {
+    "revision": "d9d91437de16831133b5",
+    "url": "/tandp/static/js/runtime~main.d9d91437.js"
+  },
+  {
+    "revision": "abfa94d751d9b72de58124d290f1e246",
+    "url": "/tandp/index.html"
+  }
+];
